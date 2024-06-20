e0101a7becd2f9a9c272fa145adc99a095ac106a
drjoeycadieux <instructor.joeycadieux@gmail.com>
2024-06-20 10:35:16 -0400

    inm.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    // ... other Webpack config
     plugins: [
         new Dotenv({
-            path: path.resolve(__dirname, '.env'), // Path to your .env file
+            path: path.resolve(__dirname, '.env'),
         }),
     ],
 };
